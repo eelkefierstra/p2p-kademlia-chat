1a47c666d9f3af8eedf131e50f6a8acd2c7d4336
MelvinKool <melvin@melvinkool.nl>
2017-08-17 11:41:05 -0400

    Simple TCP client

--- client/src/main.py
+++ client/src/main.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
+
+import p2pchat.trackerclient
+
 if __name__ == "__main__":
-    pass
+    trackerclient = p2pchat.trackerclient.TrackerClient(('localhost', 1337)) 
+    trackerclient.connect()
+    trackerclient.disconnect()
+
