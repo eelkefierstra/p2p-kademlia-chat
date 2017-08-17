de309be6054832f901df44b4900a94843c154e31
MelvinKool <melvin@melvinkool.nl>
2017-08-17 05:21:55 -0400

    Added license to setup.py

--- tracker/setup.py
+++ tracker/setup.py
@@ -8,6 +8,7 @@ setup(
         name="p2pchat-tracker",
         description="The tracker for p2pchat",
         url="https://github.com/eelkefierstra/p2p-kademlia-chat",
+        license="MIT",
         packages=find_packages(),
         install_requires=required
 )
