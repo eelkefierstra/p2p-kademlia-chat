61fcf5bf11709e8fda2967ba5d89fb0d0fb19c80
root <root@localhost.localdomain>
2017-09-28 16:26:54 -0400

    Changed from sockets to twisted

--- tracker/src/main.py
+++ tracker/src/main.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 
+import optparse
 import p2pchat.tracker
 
+def parse_args():
+    usage = """usage: %prog [options]
+
+This is the Fast Poetry Server, Twisted edition.
+Run it like this:
+
+  python main.py
+"""
+
+    parser = optparse.OptionParser(usage)
+
+    help = "The port to listen on. Default to a random available port."
+    parser.add_option('--port', type='int', help=help, default=1337)
+
+    help = "The interface to listen on. Default is localhost."
+    parser.add_option('--iface', help=help, default='localhost')
+
+    options, args = parser.parse_args()
+
+    return options
+
+
 if __name__ == "__main__":
-    bindaddr = ('127.0.0.1', 1337)
-    tracker = p2pchat.tracker.Tracker(bindaddr)
+    options = parse_args()
+    tracker = p2pchat.tracker.Tracker(options.iface, options.port)
     tracker.start()
-    tracker.stop()
