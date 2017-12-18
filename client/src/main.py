0922f5d0ad2726438441954f3213a1f6d5ec2e6e
root <root@localhost.localdomain>
2017-12-18 16:49:59 -0500

    Initial protocol setup

--- client/src/main.py
+++ client/src/main.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
+import optparse
 import p2pchat.trackerclient
 
+def parse_args():
+    usage = """Usage: %prog [options]
+
+This is the client for the p2p kademlia chat protocol.
+"""
+
+    parser = optparse.OptionParser(usage)
+
+    help = "The port to connect to. Default to 1337."
+    parser.add_option('--port', type='int', help=help, default=1337)
+
+    help = "The host to connect to."
+    parser.add_option('--host', help=help)
+
+    options, args = parser.parse_args()
+
+    return options
+
+
+
 if __name__ == "__main__":
-    trackerclient = p2pchat.trackerclient.TrackerClient(('localhost', 1337)) 
-    trackerclient.connect()
-    trackerclient.disconnect()
+    options = parse_args()
+    trackerclient = p2pchat.trackerclient.TrackerClient('localhost', 1337) 
 
--- client/src/p2pchat/trackerclient.py
+++ client/src/p2pchat/trackerclient.py
@@ -4,20 +4,60 @@
 The p2p tracker client
 """
 
-import socket
+from twisted.internet.protocol import Protocol, ClientFactory
+
+class TrackerClientProtocol(Protocol):
+
+
+    def parse_new_chat(self, chatJSON):
+        chatuuid = chatJSON["chatuuid"] 
+        #TODO do something with this new chatuuid
+        print("[*] Created new chat with uuid: {}".format(chatuuid))
+
+    def parse_messages(self, messagesJSON):
+        #TODO if fromtime >= lastmsg_time, we missed some messages.
+        # get the messages from lastmsg_time till fromtime also
+        fromtime = messagesJSON["fromtime"]
+        #TODO update the newest chat message time
+        tilltime = messagesJSON["tilltime "]
+        print("[*] Got a new message, fromtime: {}, tilltime: {}".format(fromtime,tilltime))
+    
+    def dataReceived(self, data):
+        dataJSON = json.loads(data)
+        action = dataJSON["action"]
+        if action ==  "createchat":
+            # Created new chat
+            parse_new_chat(dataJSON)
+        elif action == "getmessages":
+            # New message locations arrived
+            parse_messages(dataJSON)
+
+
+class TrackerClientFactory(ClientFactory):
+
+    def __init__(self):
+        self.lastmsg_time = 0
+
+    def buildProtocol(self, addr):
+        return TrackerClientProtocol()
+
+    def clientConnectionFailed(self, connector, reason):
+        print("[*] Connection failed: {}".format(reason))
+
+    def clientConnectionLost(self, connector, reason):
+        print("[*] Connection lost: {}".format(reason))
+
+        
+
 
 class TrackerClient:
 
-    def __init__(self, trackeraddr):
-        self.trackeraddr = trackeraddr
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
 
-    def connect(self):
-        self.trackerconn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.trackerconn.connect(self.trackeraddr)
-        self.trackerconn.send(b"Joined")
-        servermsg = self.trackerconn.recv(1024)
-        print(servermsg)
-
-    def disconnect(self):
-        self.trackerconn.send(b"Disconnect")
-        self.trackerconn.close()
+    def start(self):
+        factory = TrackerClientFactory()
+        from twisted.internet import reactor
+        reactor.connectTCP(host, port, factory)
+        reactor.run()
