bb216b6f6b70c2b885823620fa26b1cab6d47094
eelke <eelkef@gmail.com>
2017-12-28 15:47:20 +0100

    p2p should now work if working bootstrap server is available

--- client/src/p2pchat/p2pConnection.py
+++ client/src/p2pchat/p2pConnection.py
@@ -4,27 +4,31 @@
 The p2p connection frontend
 """
 
-from kademlia.network import server
+from kademlia.network import Server
+import hashlib
+
 
 class p2pConnection:
 
     def __init__(self, bootstrapAdresses):
         self.server = Server()
-        self.server.listen(50042) #Dynamic port range: 49152–65535
+        self.server.listen(8468)  # Dynamic port range: 49152-65535
 
-        for addres in bootstrapAdresses:
-            self.server.bootstrap(addres).addErrback(none) #TODO: handle error in bootstrap
+        self.server.bootstrap(bootstrapAdresses).addErrback(None)  # TODO: handle error in bootstrap
 
     def send(self, message):
-        key = 'Test' #TODO: hash message to get key to safe message to
-        self.server.set(key, message).addErrback(sendFailed) #TODO: handle error in setting message
+        m = hashlib.sha256()
+        m.update(message.to_bytes())
+        key = m.hexdigest()
+        self.server.set(key, message).addErrback(sendFailed)  # TODO: handle error in setting message
 
-    def sendFailed(err):
-        #Auto resend to network or ask user to resend?
+    def sendFailed(self, err):
+        # Auto resend to network or ask user to resend?
+        return None
 
     def get(self, key):
-        message = self.server.get(key).addErrback(none) #TODO: handle error in getting message
+        message = self.server.get(key).addErrback(None)  # TODO: handle error in getting message
         return message
 
-    def quit():
-        
+    def quit(self):
+        return None
