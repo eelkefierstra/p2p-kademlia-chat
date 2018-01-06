1b0b6b2670440aa738d742fcba6d0a64bddcd264
MelvinKool <melvin@melvinkool.nl>
2018-01-06 16:57:28 -0500

    MongoDB database connection

--- tracker/src/main.py
+++ tracker/src/main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
-import p2pchat.tracker
+import os
+from p2pchat.tracker import Tracker
+from p2pchat.database import P2PChatDB
+
+def check_file_exists(parser, filepath):
+    if not os.path.exists(filepath):
+        parser.error("The file {} does not exist!".format(arg))
+    return filepath
 
 def parse_args():
     description = """
@@ -17,6 +24,18 @@ This is the tracker server for the p2p kademlia chat protocol.
     help = "The interface to listen on. Default is localhost."
     parser.add_argument('--iface', help=help, default='localhost')
 
+    help = "The database host. Default is localhost."
+    parser.add_argument('--dbhost', help=help, default='localhost')
+
+    help = "The database port. Default is 27017."
+    parser.add_argument('--dbport', type=int, help=help, default=27017)
+
+    help = "The private key file"
+    parser.add_argument('dbprivkey', type=lambda x:check_file_exists(parser, x), help=help)
+
+    help = "The certificate file"
+    parser.add_argument('dbcert', type=lambda x: check_file_exists(parser, x), help=help)
+
     args = parser.parse_args()
 
     return args
@@ -24,5 +43,7 @@ This is the tracker server for the p2p kademlia chat protocol.
 
 if __name__ == "__main__":
     args = parse_args()
-    tracker = p2pchat.tracker.Tracker(args.iface, args.port)
+    db = P2PChatDB(args.dbhost, args.dbport, args.dbprivkey, args.dbcert)
+    db.connect()
+    tracker = Tracker(args.iface, args.port, db)
     tracker.start()
