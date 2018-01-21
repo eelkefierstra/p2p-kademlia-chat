65a451103b343c85e96891c6782d8044bcc0740d
MelvinKool <melvin@melvinkool.nl>
2018-01-21 18:55:18 -0500

    Fix merge conflict merged wrong

--- tracker/src/p2pchat/tracker.py
+++ tracker/src/p2pchat/tracker.py
@@ -80,6 +80,7 @@ class TrackerProtocol(Protocol):
             response = self.send_message(json_obj)
         elif action == "getmessages":
             response = self.get_messages(json_obj)
+
         self.transport.write(response.encode('utf-8'))
 
 
