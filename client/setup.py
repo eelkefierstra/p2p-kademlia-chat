81106973c1fe0e3b99fc4ac65884575291adfe2a
root <root@localhost.localdomain>
2018-01-28 11:36:19 -0500

    Unit testing for client

--- client/setup.py
+++ client/setup.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 from setuptools import setup, find_packages
+import unittest
+
+# Needed to locate the p2pchat package
+import sys
+sys.path.append('src/')
 
 with open("requirements.txt") as reqfile:
     required = reqfile.read().splitlines()
+
+def my_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('src/test/', pattern='test_*.py')
+    return test_suite
 
 setup(
         name="p2pchat-client",
@@ -10,6 +20,7 @@ setup(
         url="https://github.com/eelkefierstra/p2p-kademlia-chat",
         license="MIT",
         packages=find_packages(),
-        install_requires=required
+        install_requires=required,
+        test_suite='setup.my_test_suite'
 )
 
